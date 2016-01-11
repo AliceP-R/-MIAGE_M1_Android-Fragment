c1605355ad265df2bcafd529801f2bc9bdafb205
AliceP-R <alice.pottier@free.fr>
2016-01-11 09:27:48 +0100

    Ajout de 2 fragments (classe java)

---
+++ app/src/main/java/com/example/alice/fragment/PremierFrag.java
@@ -0,0 +1,21 @@
+package com.example.alice.fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Alice on 11/01/2016.
+ */
+public class PremierFrag extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.deuxieme_layout, container, false);
+    }
+}
