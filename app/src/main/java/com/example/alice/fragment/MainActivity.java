59dd16533edfb29f4ed3e0869972084ad6eb0cd2
AliceP-R <alice.pottier@free.fr>
2016-01-11 09:42:51 +0100

    ajout du premier fragment dans le FrameLayout de la première activité

--- app/src/main/java/com/example/alice/fragment/MainActivity.java
+++ app/src/main/java/com/example/alice/fragment/MainActivity.java
@@ -1,15 +1,30 @@
 package com.example.alice.fragment;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(findViewById(R.id.FrameL) != null)
+        {
+            if(savedInstanceState != null)
+                return;
+            PremierFrag frag1 = new PremierFrag();
+            // FragmentTransaction permet d'ajouter ou d'enlever des fragments d'une vue.
+            FragmentTransaction transaction = getFragmentManager().beginTransaction();
+            // on ajoute un PremierFrag dans le FrameLayout du main
+            transaction.add(R.id.FrameL, frag1);
+            // optionnel : sauvegarde dans le backstack l'ajout du frag1
+            transaction.addToBackStack(null);
+            // obligatoire : si on ne commit pas, il ne se passe rien
+            transaction.commit();
+        }
     }
 
     public void onClickButton(View v)
